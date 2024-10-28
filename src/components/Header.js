923a9645b3a360c8e709dc19b4dab4e2068f4c05
sandeep <sandeep040949@gmail.com>
2024-10-28 11:09:07 +0530

    loginIn and SignUp Form

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+
+const Header=()=>{
+    return(
+        <div className="w-48 px-8 py-2 bg-gradient-to-b from-black absolute  top-0 left-28 ">
+          <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="netflix-logo"/>
+        </div>
+    )
+}
+export default Header;
